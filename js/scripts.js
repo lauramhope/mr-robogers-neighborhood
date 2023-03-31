287f15bf42f3d03e2467ce84e9f32097f41b5305
Laura Hope <ssteamlaura@gmail.com>
2023-03-31 15:45:53 -0700

    Add template to HTML, begin writing functions in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,100 +1,127 @@
-
-let userInput = [];
-function countUp(stopNumber) {
-  for (let i = 0; i <= stopNumber; i++) {
-    userInput.push(i);
-  }
-  return userInput.toString(); 
-}
-
-
-function mrRobo(input) {
-  const num1 = ['1'];
-  const num2 = ['2'];
-  const num3 = ['3'];
-  let results = ''; 
-  
-  for (let i = 0; i <input.length; i++) {
-    if (input.includes(num3)) {
-      return "won't you be my neighbor?";
-    } else if (input.includes(num2)) {
-      return "boop!"; 
-    } else if (input.includes(num1)) {
-      return "beep!"
-    } else {
-      return input; 
-    }
-  }
-  return results;
-}
-
-
-function mrRobo(input) {
+function mrRobo(event) {
+  event.preventDefault();
   const num1 = '1';
   const num2 = '2';
-  const num3 = '3';
-  let results = ''; 
-  
-  for (let i = 0; i <input.length; i++) {
-    if (input.includes('3')) {
-      return "won't you be my neighbor?";
-    } else if (input.includes('2')) {
-      return "boop!"; 
-    } else if (input.includes('1')) {
-      return "beep!"
-    } else {
-      return input; 
-    }
-  }
-  return results;
-}
-
-userInput.toString().replaceAll('1','beep');
-
-
-__________________
-function mrRobo(input) {
-  const num1 = '1';
-  const num2 = '2';
-  const num3 = '3';
-  let results = '';
-
+  const num3 = '3'; 
+  const inputNum = document.querySelector("input#number").value;
+  let result;
   let newArray = [];
+
   function countUp(input) {
-    for (let i = 0; i<= input; i++) {
-      newArray.push(i);
+    for(let i = 0; i <= input; i++) {
+      newArray.push(i); 
     }
-    return newArray.toString();
+    return newArray;
   }
 
-  for (let i = 0; i <= input.length; i++) {
-    if (input.includes(num3)) {
-      return "won't you be my neighbor?";
-    } else if (input.includes(num2)) {
-      return "boop!";
-    } else if (input.includes(num1)) {
-      return "beep!";
-    } else {
-      return input;
-    }
-  }
-  return results; 
+  document.getElementById("answer").innerText = result; 
 }
-____ mrRobo('14'); = 'beep!'
 
-let newArray = [];
-function countUp(input) {
-  for (let i = 0; i <= input; i++) {
-    newArray.push(i);
-  } if (input.includes('3')) {
-    return "won't you be my neighbor?"; 
-  } else if (input.includes('2')) {
-    return "boop";
-  } else if (input.includes('1')) {
-    return "beep"; 
-  } else {
-    return input;
-  }
-  return newArray; 
-}
-------- countUp('1'); 'beep!'
+
+Window.addEventListener("load", function() {
+  const form = document.getElementById("survey");
+  form.addEventListener("submit, mrRobo")
+}); 
+
+
+
+
+// let userInput = [];
+// function countUp(stopNumber) {
+//   for (let i = 0; i <= stopNumber; i++) {
+//     userInput.push(i);
+//   }
+//   return userInput.toString(); 
+// }
+
+
+// function mrRobo(input) {
+//   const num1 = ['1'];
+//   const num2 = ['2'];
+//   const num3 = ['3'];
+//   let results = ''; 
+  
+//   for (let i = 0; i <input.length; i++) {
+//     if (input.includes(num3)) {
+//       return "won't you be my neighbor?";
+//     } else if (input.includes(num2)) {
+//       return "boop!"; 
+//     } else if (input.includes(num1)) {
+//       return "beep!"
+//     } else {
+//       return input; 
+//     }
+//   }
+//   return results;
+// }
+
+
+// function mrRobo(input) {
+//   const num1 = '1';
+//   const num2 = '2';
+//   const num3 = '3';
+//   let results = ''; 
+  
+//   for (let i = 0; i <input.length; i++) {
+//     if (input.includes('3')) {
+//       return "won't you be my neighbor?";
+//     } else if (input.includes('2')) {
+//       return "boop!"; 
+//     } else if (input.includes('1')) {
+//       return "beep!"
+//     } else {
+//       return input; 
+//     }
+//   }
+//   return results;
+// }
+
+// userInput.toString().replaceAll('1','beep');
+
+
+// __________________
+// function mrRobo(input) {
+//   const num1 = '1';
+//   const num2 = '2';
+//   const num3 = '3';
+//   let results = '';
+
+//   let newArray = [];
+//   function countUp(input) {
+//     for (let i = 0; i<= input; i++) {
+//       newArray.push(i);
+//     }
+//     return newArray.toString();
+//   }
+
+//   for (let i = 0; i <= input.length; i++) {
+//     if (input.includes(num3)) {
+//       return "won't you be my neighbor?";
+//     } else if (input.includes(num2)) {
+//       return "boop!";
+//     } else if (input.includes(num1)) {
+//       return "beep!";
+//     } else {
+//       return input;
+//     }
+//   }
+//   return results; 
+// }
+// ____ mrRobo('14'); = 'beep!'
+
+// let newArray = [];
+// function countUp(input) {
+//   for (let i = 0; i <= input; i++) {
+//     newArray.push(i);
+//   } if (input.includes('3')) {
+//     return "won't you be my neighbor?"; 
+//   } else if (input.includes('2')) {
+//     return "boop";
+//   } else if (input.includes('1')) {
+//     return "beep"; 
+//   } else {
+//     return input;
+//   }
+//   return newArray; 
+// }
+// ------- countUp('1'); 'beep!'
